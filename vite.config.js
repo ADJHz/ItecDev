7d6d748a35881fb169b6d74ed6b3c887b7f32849
Adolfo <adolfodjhz@gmail.com>
2026-04-09 20:03:52 -0600

    correccion
    correccion

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
+import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
     ],
-    build: {
-        outDir: "dist",
-        emptyOutDir: true,
-    },
 });
